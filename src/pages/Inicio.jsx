36a660539c9e06d89f816a7da2cb8dd6538d9c27
Helder Cambuta <heldercambuta44@gmail.com>
2023-01-19 10:10:37 +0100

    🚀

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,4 +1,4 @@
-
+import api from "../service"
 import Hero from "../components/Hero"
 import Recentes from "../components/Recentes"
 import Animes from "../components/Animes"
@@ -6,8 +6,18 @@ import Filmes from "../components/Filmes"
 import Jogos from "../components/Jogos"
 import Series from "../components/Series"
 import Footer from "../components/Footer"
+import { useState } from "react"
 
 const Inicio = () => {
+
+  const [posts, setPosts] = useState()
+
+  //api.get('/postagens')
+  //  .then((response) => { 
+  //    console.log(response)
+  //    setPosts(response.data)
+  //  })
+
   return (
     <>
       <Hero />
